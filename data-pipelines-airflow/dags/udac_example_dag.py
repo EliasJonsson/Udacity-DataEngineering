d8985e56806e94d302630ca9c0719a6c9b42ff0c
EliasJonsson <ellijons7@gmail.com>
2021-05-22 10:20:24 +0000

    Feat: add s3 to redshift operator

--- data-pipelines-airflow/dags/udac_example_dag.py
+++ data-pipelines-airflow/dags/udac_example_dag.py
@@ -26,12 +26,23 @@ start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id='Stage_events',
-    dag=dag
+    dag=dag,
+    table="events",
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    s3_bucket="udacity-dend",
+    s3_key="",
+    json_format="s3://udacity-dend/log_json_path.json",
 )
 
 stage_songs_to_redshift = StageToRedshiftOperator(
     task_id='Stage_songs',
-    dag=dag
+    dag=dag,
+    table="events",
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    s3_bucket="udacity-dend",
+    s3_key="",
 )
 
 load_songplays_table = LoadFactOperator(
